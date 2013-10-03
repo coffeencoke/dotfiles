9f6eb0059c1cf108b999837a47b11a6bcd997d70
Matt Simpson <matt.simpson3@gmail.com>
2013-10-02 22:19:18 -0500

    Add directories for common projects

--- projects/aliases.bash
+++ projects/aliases.bash
@@ -1,1 +1,7 @@
 function c { cd ~/Projects/$1; }
+
+alias aboutyou="cd ~/Projects/magnet/aboutyou"
+alias testcenter="cd ~/Projects/magnet/test_center_server"
+alias kraken="cd ~/Projects/magnet/kraken"
+alias admin="cd ~/Projects/magnet/magnet_tester_admin"
+alias api_docs="cd ~/Projects/magnet/api_docs"
