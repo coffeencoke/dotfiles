ba340ffc9795ab8a50b53286663d6d30670cfdc3
Matt Simpson <matt.simpson3@gmail.com>
2013-11-02 01:08:56 -0500

    Add bash_alias cookbook

---
+++ cookbooks/bash_alias/metadata.rb
@@ -0,0 +1,8 @@
+name 'bash_alias'
+description 'Creates shell aliases for a user using bash'
+version '0.1.0'
+supports 'osx', '10.9' # this is the only OS that it is tested in
+maintainer_email 'matt.simpson3[at]gmail.com'
+maintainer 'Matt Simpson'
+license 'MIT'
+
