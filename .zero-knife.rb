2757e82b049dfae42e4397a44aecff115b6deac5
Matt Simpson <matt.simpson3@gmail.com>
2013-12-15 17:53:43 -0600

    Update ignore list

--- .zero-knife.rb
+++
@@ -1,3 +0,0 @@
-            chef_server_url 'http://192.168.200.10:4000'
-            node_name 'zero-host'
-            client_key '/var/folders/pl/4rrrz3tx26z4z1zzhq2xp3p80000gq/T/fake_key/fake.pem'
