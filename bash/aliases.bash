088627c0d37ae6c6ee833dcf90ab5a807d17e627
Matt Simpson <matt.simpson3@gmail.com>
2013-12-16 22:41:18 -0600

    update dotfiles

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -42,7 +42,7 @@ alias mv='mv -v'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 #alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-alias alert='terminal-notifier -title "Command finished" -message "Your command has finished running"'
+alias alert='terminal-notifier -title "Command finished" -message "Your command has finished running" -sender com.apple.Terminal'
 
 # Fully update operating system
 if [ -f /etc/debian_version ]; then
