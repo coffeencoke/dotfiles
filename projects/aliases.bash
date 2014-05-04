3c2ba5b17045bff4c026b9c2e89d585d64f7501f
Matt Simpson <matt.simpson3@gmail.com>
2014-05-04 16:49:38 -0700

    Remove projects aliases

--- projects/aliases.bash
+++
@@ -1,5 +0,0 @@
-export PROJECT_DIR="~/Projects/"
-function c { cd ~/$PROJECT_DIR/$1; }
-
-alias personal_mode="export GEM_HOME=~/.gems; export GEM_PATH=~/.gems; export PATH=~/.gems/bin:$PATH"
-alias work_mode="unset GEM_HOME; unset GEM_PATH;"
